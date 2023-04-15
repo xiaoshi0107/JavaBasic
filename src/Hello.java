8a9c8840d9440a545b31ff14ff00b16218669701
xshi <stushi0107@gmail.com>
2023-04-15 10:51:40 +0800

    init

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        Scanner in = new Scanner(System.in);
+        int amount, price;
+        System.out.print("please input the amount:");
+        amount = in.nextInt();
+        System.out.print("please input the price:");
+        price = in.nextInt();
+//        System.out.println("This is your input:" + in.nextLine());
+        System.out.println(amount + "-" + price + "=" + (amount - price));
     }
 }
